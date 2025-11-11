1aad0bcca1d9ade05a43c825bc016f7240322ce7
dantutor <danmbaict@gmail.com>
2025-11-11 09:44:14 +0300

    fix: compatible i18n config import for Vercel build

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'am'],
+    locales: ['en', 'am','om'],
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 // next.config.mjs
-import nextI18NextConfig from './next-i18next.config.js';
+import pkg from './next-i18next.config.js';
+const { i18n } = pkg;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...nextI18NextConfig, // ✅ spread the whole config (works with CommonJS)
+  i18n,
   reactStrictMode: true,
 };
 
